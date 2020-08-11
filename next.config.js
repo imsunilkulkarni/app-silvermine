96ba14779b4f09cd85fe298518027967bd5fa5a1
sunilkulkarnink@gmail.com <sunilkulkarnink@gmail.com>
2020-08-11 12:52:02 +0530

    modiied config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
 
-module.exports = withSass({url: false,})
-module.exports = withCSS({url: false,})
+
+module.exports = withSass({
+    
+})
+
